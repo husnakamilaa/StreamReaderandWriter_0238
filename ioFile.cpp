34fcb7ea6d7b9d8da73828244df3134fb15bdbfa
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-06-04 15:02:06 +0700

    menambahkan seluruh perintah untuk membaca

--- ioFile.cpp
+++ ioFile.cpp
@@ -27,4 +27,27 @@ int main() {
 
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+
+    }
+    else cout << "unable to open file";
+
+    return 0;
 }
